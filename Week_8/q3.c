0efa98fb1c9391ea2aa53ada013f1a96444475ce
komalboyo <91676569+komalboyo@users.noreply.github.com>
2024-09-16 10:55:14 +0530

    Update q3.c

--- Week_8/q3.c
+++ Week_8/q3.c
@@ -5,20 +5,20 @@
 
 sem_t s1, s2;
 
-void *func1(void *p) {
+void *f1() {
 	sem_wait(&s1);
-	printf("Func 1 acquired s1");
+	printf("Func 1 acquired s1\n");  //needs \n to print
 	sem_wait(&s2);
-	printf("Func 1 acquired s2");
+	printf("Func 1 acquired s2\n");
 	printf("Thread 1!\n");
 	sem_post(&s1);
 }
 
-void *func2(void *p) {
+void *f2() {
 	sem_wait(&s2);
-	printf("Func 2 acquired s1");
+	printf("Func 2 acquired s1\n");
 	sem_wait(&s1);
-	printf("Func 2 acquired s2");
+	printf("Func 2 acquired s2\n");
 	printf("Thread 2!\n");
 	sem_post(&s2);
 }
@@ -27,10 +27,15 @@ int main() {
 	pthread_t threads[2];
 	sem_init(&s1, 0, 1);
 	sem_init(&s2, 0, 1);
-	pthread_create(&threads[0], 0, &func1, 0);
-	pthread_create(&threads[1], 0, &func2, 0);
+	pthread_create(&threads[0], 0, &f1, 0);
+	pthread_create(&threads[1], 0, &f2, 0);
 	pthread_join(threads[0], 0);
 	pthread_join(threads[1], 0);
 	sem_destroy(&s1);
 	sem_destroy(&s2);
 }
+
+/*OUTPUT
+Func 1 acquired s1
+Func 2 acquired s1
+*/
